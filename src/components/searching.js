cab3a40d4658f9f93dc6ead5ccdad99996055545
Dmitry Fedotochkin <dkrouli@yandex.ru>
2026-02-25 09:59:44 +0300

    1

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,11 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+const comparator = createComparison(rules.skipEmptyTargetValues);
+  
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+
+        return data.filter(row => comparator(row, state));
     }
 }
